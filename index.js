483107ac0cd96363dbf676d5b952c7f11b97f738
Mike North <michael.l.north@gmail.com>
2018-11-12 16:15:33 -0800

    fix: remove git commit of changelog to master
    Removing this code allows for use of this sharable config with protected master branches

--- index.js
+++ index.js
@@ -5,12 +5,6 @@ module.exports = {
       path: '@semantic-release/changelog',
       changelogFile: 'CHANGELOG.md'
     },
-    {
-      path: '@semantic-release/git',
-      assets: ['package.json', 'CHANGELOG.md', 'docs/**', 'etc/**'],
-      message:
-        'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-    },
     '@semantic-release/npm'
   ],
   publish: ['@semantic-release/npm', '@semantic-release/github'],
